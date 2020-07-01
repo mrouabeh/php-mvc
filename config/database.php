74326f455898b869344c74709a30df41398a142a
mrouabeh <mohamed.rouabehi.pro@gmail.com>
2020-07-02 00:33:17 +0200

    First commit

---
+++ config/database.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'php_mvc');
+define('DB_USER', 'homestead');
+define('DB_PASS', 'secret');
+define('DB_PORT', '33060');
+
+define('DSN', "mysql:host=".DB_HOST.";dbname=".DB_NAME.";port=".DB_PORT.";charset=utf8");
---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require dirname(__DIR__) . "/vendor/autoload.php";
+
+use App\Core\Router;
+
+$router = new Router;
+
+$router->get('/', "HomeController@index");
+
+$router->run();
