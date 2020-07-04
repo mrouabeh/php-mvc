7fee18cb5a2d087335976fc5f19d09c27b70481b
mrouabeh <mohamed.rouabehi.pro@gmail.com>
2020-07-04 12:05:57 +0200

    Adding Database Utility Class

--- app/Utility/Database.php
+++ app/Utility/Database.php
@@ -7,18 +7,17 @@ use PDO;
 
 class Database
 {
-    static protected $db;
+    static private $_db = null;
+    private $_PDO;
+    private $_results = [];
+    private $_error;
+    private $_count;
 
     public function __construct()
     {
-        self::init();
-    }
-
-    public static function init()
-    {
         try
         {
-            self::$db = new PDO(
+            $this->_PDO = new PDO(
                 DSN,
                 DB_USER,
                 DB_PASS,
@@ -34,12 +33,186 @@ class Database
         }
     }
 
+    /**
+     * @return Database|null
+     */
     public static function getInstance()
     {
-        if (!isset(self::$db))
+        if (!isset(self::$_db))
         {
-            self::init();
+            self::$_db = new Database();
         }
-        return (self::$db);
+        return (self::$_db);
+    }
+
+    /**
+     *
+     */
+    private function resetAttributes()
+    {
+        $this->_count = 0;
+        $this->_results = [];
+        $this->_error = false;
+    }
+
+    /**
+     * @return mixed
+     */
+    private function error()
+    {
+        return $this->_error;
+    }
+
+    /**
+     * @param $sql
+     * @param array $params
+     * @return $this
+     */
+    public function query($sql, array $params = [])
+    {
+        $this->resetAttributes();
+        if ($_stmt = $this->_PDO->prepare($sql))
+        {
+            foreach ($params as $key => $value)
+            {
+                $_stmt->bindValue($key, $value);
+            }
+            if ($_stmt->execute())
+            {
+                $this->_results = $_stmt->fetchAll(PDO::FETCH_OBJ);
+                $this->_count = $_stmt->rowCount();
+            }
+            else
+            {
+                $this->_error = true;
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * @param $action
+     * @param $table
+     * @param array $where
+     * @return $this|bool
+     */
+    public function action($action, $table, array $where = [])
+    {
+        if (count($where) == 3)
+        {
+            $operator = $where[1];
+            $operators = ["=", ">", "<", ">=", "<="];
+            if (in_array($operator, $operators))
+            {
+                $field = $where[0];
+                $value = $where[2];
+                $params = [":value" => $value];
+                if (!$this->query("{$action} FROM `{$table}` WHERE `{$field}` {$operator} :value", $params)->error())
+                {
+                    return $this;
+                }
+            }
+        }
+        else
+        {
+            if (!$this->query("{$action} FROM `{$table}`")->error())
+            {
+                return $this;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @param $table
+     * @param array $where
+     * @return $this|bool
+     */
+    public function select($table, array $where = [])
+    {
+        return $this->action("SELECT *", $table, $where);
+    }
+
+    /**
+     * @param $table
+     * @param array $fields
+     * @return bool
+     */
+    public function insert($table, array $fields)
+    {
+        if (count($fields))
+        {
+            $params = [];
+            foreach ($fields as $key => $value)
+            {
+                $params[":{$key}"] = $value;
+            }
+            $columns = implode(", ", array_keys($fields));
+            $values = implode(", ", array_keys($params));
+            if (!$this->query("INSERT INTO `{$table}` (`{$columns}`) VALUES({$values})", $params)->error())
+            {
+                return $this->_PDO->lastInsertId();
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @param $table
+     * @param $id
+     * @param array $fields
+     * @return bool
+     */
+    public function update($table, $id, array $fields)
+    {
+        if (count($fields)) {
+            $x = 1;
+            $set = "";
+            $params = [];
+            foreach ($fields as $key => $value) {
+                $params[":{$key}"] = $value;
+                $set .= "`{$key}` = :$key";
+                if ($x < count($fields)) {
+                    $set .= ", ";
+                }
+                $x ++;
+            }
+            if (!$this->query("UPDATE `{$table}` SET {$set} WHERE `id` = {$id}", $params)->error()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @param $table
+     * @param array $where
+     * @return $this|bool
+     */
+    public function delete($table, array $where = [])
+    {
+        return $this->action("DELETE", $table, $where);
+    }
+
+    /**
+     * @param null $key
+     * @return array|mixed
+     */
+    public function results($key = null) {
+        return(isset($key) ? $this->_results[$key] : $this->_results);
+    }
+
+    /**
+     * @return array|mixed
+     */
+    public function first() {
+        return($this->results(0));
+    }
+
+    /**
+     * @return mixed
+     */
+    public function count() {
+        return($this->_count);
     }
 }
